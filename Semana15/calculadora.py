3361b8cacb50b849aa4ce944ea93153dc224a477
juanlopezDeveloper <39016283+juanlopezDeveloper@users.noreply.github.com>
2018-06-05 00:50:03 -0500

    calculadora_documentacion.oy
    Hola profe. Envió documentación del código calculadora

--- Semana15/calculadora.py
+++ Semana15/calculadora.py
@@ -1,9 +1,20 @@
+# Importamos las aplicaciones sys, PyQt5, QtWidgets, Uic y QtGui
 import sys
 from PyQt5 import QtWidgets, uic, QtGui
 
 class MiVentana(QtWidgets.QDialog):
+    """ Esta clase es de una interfaz que tiene atributos para ser una calculadora"""
 
     def operar(self, n1, n2, tipo):
+        """
+        int -> int
+        Función para realizar operaciones básicas de calculadora.
+        
+        :param n1: Número entero
+        :param n2: Número entero
+        :param tipo: booleano
+        :return: Nḿuero entero
+        """
         if tipo == 'Suma':
             return n1 + n2
         elif tipo == 'Resta':
@@ -16,6 +27,10 @@ class MiVentana(QtWidgets.QDialog):
             return n1 / n2
 
     def btn_operar(self):
+        """
+        Permite digitar números decimales.
+        :return: Número decimal.
+        """
         try:
             n1 = float(self.num1.text())
             n2 = float(self.num2.text())
@@ -26,6 +41,7 @@ class MiVentana(QtWidgets.QDialog):
             self.resultado.setText('Datos no númericos')
 
     def __init__(self):
+        """Instancia que importa interfaz de PyQt5 con forma de calculadora"""
         super(MiVentana, self).__init__()
         uic.loadUi('calculadora.ui',self)
         self.oper.addItems([
@@ -38,6 +54,7 @@ class MiVentana(QtWidgets.QDialog):
         self.show()
 
 if __name__ == '__main__':
+    """Importa las graficas de la calculadora"""
     app = QtWidgets.QApplication(sys.argv)
     window = MiVentana()
     sys.exit(app.exec_())
